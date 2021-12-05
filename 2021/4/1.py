72d203e0738eda1f159eaf1ef06204b747a82819
Daniel Lantz <daniel@kodifi.ai>
2021-12-05 17:05:31 -0500

    day 4

---
+++ 2021/4/1.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+from typing import List, NamedTuple
+
+BOARD_DIMENSION = 5
+
+
+class Number(NamedTuple):
+    value: str
+    marked: bool
+
+
+Row = List[Number]
+Board = List[Row]
+
+
+def mark_drawn_number(n: str, board: Board):
+    for x, row in enumerate(board):
+        for y, number in enumerate(row):
+            if number.value == n:
+                board[x][y] = Number(n, True)
+
+
+def check_for_bingo(board: Board):
+    return any(
+        [
+            # lateral
+            any([all([n.marked for n in row]) for row in board]),
+            # vertical
+            any(
+                [all([row[i].marked for row in board]) for i in range(BOARD_DIMENSION)]
+            ),
+            # diag
+            # all([board[i][i].marked for i in range(BOARD_DIMENSION)]),
+            # reverse diag
+            # all(
+            #     [
+            #         board[i][BOARD_DIMENSION - (i + 1)].marked
+            #         for i in range(BOARD_DIMENSION)
+            #     ]
+            # ),
+        ]
+    )
+
+
+def get_board_score(board: Board, last_draw_str: str) -> int:
+    last_draw = int(last_draw_str)
+
+    sum_unmarked = 0
+    for row in board:
+        for n in row:
+            if not n.marked:
+                sum_unmarked += int(n.value)
+
+    print(f"sum unmarked: {sum_unmarked}")
+
+    return sum_unmarked * last_draw
+
+
+with open("2021/4/input.txt") as f:
+    sections = f.read().split("\n\n")
+
+    draw_numbers = sections[0].split(",")
+
+    boards: List[Board] = []
+
+    for board_str in sections[1:]:
+        boards.append(
+            [[Number(n, False) for n in row.split()] for row in board_str.split("\n")]
+        )
+
+    for n in draw_numbers:
+        print(n)
+        for board in boards:
+            mark_drawn_number(n, board)
+            if check_for_bingo(board):
+                print(f"found bingo on board {board}")
+                score = get_board_score(board, n)
+                print(f"score: {score}")
+                exit()
