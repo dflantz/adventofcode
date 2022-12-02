4a92aef19e39f7d287b7d2ae418b45602ab7bba2
Daniel Lantz <daniel.lantz@trovata.io>
2022-12-02 12:16:51 -0500

    day 2 part 1

---
+++ 2022/day02a/src/main.rs
@@ -0,0 +1,63 @@
+enum Move {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+fn get_move(s: &str) -> Move {
+    match s {
+        "A" => Move::Rock,
+        "B" => Move::Paper,
+        "C" => Move::Scissors,
+
+        "X" => Move::Rock,
+        "Y" => Move::Paper,
+        "Z" => Move::Scissors,
+
+        _ => unreachable!(),
+    }
+}
+
+fn calculate_points(their_move: Move, your_move: Move) -> usize {
+    let move_points = match your_move {
+        Move::Rock => 1,
+        Move::Paper => 2,
+        Move::Scissors => 3,
+    };
+
+    let match_points = match your_move {
+        Move::Rock => match their_move {
+            Move::Paper => 0,
+            Move::Rock => 3,
+            Move::Scissors => 6,
+            _ => unreachable!(),
+        },
+        Move::Paper => match their_move {
+            Move::Paper => 3,
+            Move::Rock => 6,
+            Move::Scissors => 0,
+            _ => unreachable!(),
+        },
+        Move::Scissors => match their_move {
+            Move::Paper => 6,
+            Move::Rock => 0,
+            Move::Scissors => 3,
+            _ => unreachable!(),
+        },
+        _ => unreachable!(),
+    };
+
+    move_points + match_points
+}
+
+fn main() {
+    println!(
+        "{}",
+        include_str!("../input.txt")
+            .lines()
+            .map(|n| n.split_once(" ").unwrap())
+            .map(|(them, you)| (get_move(them), get_move(you)))
+            .map(|(them, you)| calculate_points(them, you))
+            .sum::<usize>(),
+    );
+}
