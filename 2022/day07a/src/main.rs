c550281a3b5b0837afac59c3f2da358bb02ba31d
Daniel Lantz <daniel.lantz@trovata.io>
2022-12-07 13:17:46 -0500

    code runs but wrong solution

--- 2022/day07a/src/main.rs
+++ 2022/day07a/src/main.rs
@@ -1,36 +1,89 @@
-use std::collections::HashMap;
+use std::{borrow::Borrow, cell::RefCell, collections::HashMap, rc::Rc};
 
+struct Fs<'a> {
+    dirs: Vec<Directory<'a>>,
+}
+
+impl Fs<'_> {
+    fn get_dir_sum(&self, index: usize) -> usize {
+        let mut sum = 0;
+        for i in &self.dirs[index].child_directory_indices {
+            sum += self.get_dir_sum(*i)
+        }
+        return sum + self.dirs[index].file_sizes.iter().sum::<usize>();
+    }
+}
 struct Directory<'a> {
-    child_directories: HashMap<&'a str, Directory<'a>>,
+    dir_name: &'a str,
+    child_directory_indices: Vec<usize>,
     file_sizes: Vec<usize>,
-    parent_directory: Option<&'a Directory<'a>>,
+    index: usize,
+    parent_directory_index: Option<usize>,
 }
 
 fn main() {
-    let mut root = Directory {
-        child_directories: HashMap::new(),
-        file_sizes: Vec::new(),
-        parent_directory: None,
+    let mut fs = Fs {
+        dirs: vec![Directory {
+            dir_name: "/",
+            child_directory_indices: Vec::new(),
+            file_sizes: Vec::new(),
+            index: 0,
+            parent_directory_index: None,
+        }],
     };
 
-    let curr_dir_pointer = &mut root;
+    let mut i = 0;
 
     for l in include_str!("../input.txt").lines() {
-        let s = l.split(" ").next().unwrap();
-        print!("{}: ", s);
+        let mut words = l.split(" ");
+        let s = words.next().unwrap();
         match s {
-            "$" => println!("Run command"),
-            "dir" => {
-                curr_dir_pointer.child_directories.insert(
-                    s,
-                    Directory {
-                        child_directories: HashMap::new(),
-                        file_sizes: Vec::new(),
-                        parent_directory: None,
-                    },
-                );
+            "$" => {
+                let word = words.next().unwrap();
+                match word {
+                    "cd" => {
+                        let target = words.next().unwrap();
+                        match target {
+                            ".." => {
+                                i = fs.dirs[i].parent_directory_index.unwrap();
+                            }
+                            "/" => {
+                                i = 0;
+                            }
+                            _ => {
+                                i = fs.dirs.iter().position(|d| d.dir_name == target).unwrap();
+                            }
+                        }
+                    }
+                    _ => (),
+                }
             }
-            _ => println!("Add Entry to Dir"),
+            "dir" => {
+                let d = Directory {
+                    dir_name: words.next().unwrap(),
+                    child_directory_indices: Vec::new(),
+                    file_sizes: Vec::new(),
+                    index: fs.dirs.len(),
+                    parent_directory_index: Some(i),
+                };
+                let len = fs.dirs.len();
+                fs.dirs[i].child_directory_indices.push(len);
+                fs.dirs.push(d);
+            }
+            _ => fs.dirs[i].file_sizes.push(s.parse::<usize>().unwrap()),
         };
     }
+
+    // let answer = 0;
+    // for i in 0..fs.dirs.len() {
+    //     dir_sum = fs.get_dir_sum(i);
+    //     if dir_sum > 100000 {}
+    // }
+    let ans = fs.dirs
+        .iter()
+        .enumerate()
+        .map(|(i, _)| fs.get_dir_sum(i))
+        .filter(|x| *x <= 100000)
+        .sum::<usize>();
+    println!("{}", ans);
 }
