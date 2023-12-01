8b14bc2e364c6798cc74a964be878638b4fcbf8c
Daniel Lantz <daniellantz@macbook-pro.myfiosgateway.com>
2023-11-30 19:36:02 -0500

    2023 concept

---
+++ 2023/README.md
@@ -0,0 +1,5 @@
+### Advent of Code 2023: Variables are for the weak
+
+Python, but any instances of `' = '` cause instant failure
+
+Usage: `./run.sh <path-to-solution>.py`
---
+++ 2023/run.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Check if a filename is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <python_file>"
+    exit 1
+fi
+
+filename="$1"
+
+cat << 'EOF'
+                 ,#####,
+                 #_   _#
+                 |a` `a|
+                 |  u  |
+                 \  =  /
+                 |\___/|
+        ___ ____/:     :\____ ___
+      .'   `.-===-\   /-===-.`   '.
+     /      .-"""""-.-"""""-.      \
+    /'             =:=             '\
+  .'  ' .:    o   -=:=-   o    :. '  `.
+  (.'   /'. '-.....-'-.....-' .'\   '.)
+  /' ._/   ".     --:--     ."   \_. '\
+ |  .'|      ".  ---:---  ."      |'.  |
+ |  : |       |  ---:---  |       | :  |
+  \ : |       |_____._____|       | : /
+  /   (       |----|------|       )   \
+ /... .|      |    |      |      |. ...\
+|::::/'' jgs /     |       \     ''\::::|
+'""""       /'    .L_      `\       """"'
+           /'-.,__/` `\__..-'\
+          ;      /     \      ;
+          :     /       \     |
+          |    /         \.   |
+          |`../           |  ,/
+          ( _ )           |  _)
+          |   |           |   |
+          |___|           \___|
+          :===|            |==|
+           \  /            |__|
+           /\/\           /"""`8.__
+           |oo|           \__.//___)
+           |==|
+           \__/
+EOF
+
+echo "$functional_chad"
+
+if grep -n " = " "$filename"; then
+    echo -e "\nWhat's this? Declaring variables again?\nYou're WEAK! And your CODE is WEAK!\n"
+    exit 1
+else
+    echo "\Awesome code. Nice size. Looking thick. Solid. Tight. Keep me posted on your progress.\n"
+    python "$filename"
+fi
