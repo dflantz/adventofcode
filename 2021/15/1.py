2dfe810dbcac9dfa7693396fdbfba42cb8b62742
Daniel Lantz <daniellantz@macbook-pro.myfiosgateway.com>
2023-12-04 08:43:50 -0500

    max recursion depth

--- 2021/15/1.py
+++ 2021/15/1.py
@@ -1,40 +1,51 @@
+from collections import defaultdict
 import sys
 
 if __name__ == "__main__":
-    with open("15/input.txt", "r") as f:
+    with open("input.txt", "r") as f:
         coords = list(map(lambda s: [int(c) for c in s.strip()], f.readlines()))
-        y_max = len(coords)
-        x_max = len(coords[0])
+        y_max = len(coords) - 1
+        x_max = len(coords[0]) - 1
 
-    visited = [[False for _ in range(x_max)] for _ in range(y_max)]
-    distance_from_source = [[sys.maxsize for _ in range(x_max)] for _ in range(y_max)]
+    distance_from_source = defaultdict(lambda: sys.maxsize)
 
-    visited[0][0] = True
-
-    def traverse(current_position=(0, 0), distance=0):
-        visited[current_position[0]][current_position[1]] = True
-
-        if current_position == (y_max, x_max):
+    def traverse(
+        current_position=(y_max, x_max),
+        distance=0,
+        visited=defaultdict(bool),
+    ):
+        distance += coords[current_position[0]][current_position[1]]
+        visited[current_position[0], current_position[1]] = True
+        if distance < distance_from_source[current_position]:
+            distance_from_source[current_position] = distance
+        else:
             return
-        unvisited_neighbors = []
+        if current_position == (0, 0):
+            return
 
         for move in ((0, 1), (0, -1), (1, 0), (-1, 0)):
-            y, x = current_position[0] + move[0], current_position[1] + move[1]
-            if 0 <= y < y_max and 0 <= x < x_max and not visited[y][x]:
-                curr_distance = distance + coords[y][x]
-                distance_from_source[y][x] = min(
-                    curr_distance, distance_from_source[y][x]
-                )
-                unvisited_neighbors.append(((y, x), distance_from_source[y][x]))
+            candidate_coords = (
+                current_position[0] + move[0],
+                current_position[1] + move[1],
+            )
+            if (
+                0 <= candidate_coords[0] <= y_max
+                and 0 <= candidate_coords[1] <= x_max
+                and not visited[candidate_coords]
+            ):
+                try:
+                    traverse(
+                        candidate_coords,
+                        distance,
+                        visited.copy(),
+                    )
+                except IndexError:
+                    pass
 
-        if unvisited_neighbors:
-            neighbor = sorted(unvisited_neighbors, key=lambda x: x[1])[0]
-            traverse(neighbor[0], neighbor[1])
-        else:
-            print("all neighbors visited for {current_position}")
+    visited = defaultdict(bool)
+    visited[y_max, x_max] = True
+    for move in ((0, -1), (-1, 0)):
+        candidate_coords = (y_max + move[0], x_max + move[1])
+        traverse(candidate_coords, distance=0, visited=visited)
 
-    traverse()
-
-    # print(visited)
-    for l in distance_from_source:
-        print(l)
+    print(distance_from_source[0, 0])
