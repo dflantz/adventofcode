28b2923ca13dbccc524576be63bca23d6f53c822
Daniel Lantz <daniellantz@macbook-pro.myfiosgateway.com>
2023-12-04 22:17:44 -0500

    introduce optimization back to p1

--- 2023/04/1.py
+++ 2023/04/1.py
@@ -1,10 +1,5 @@
 from pipe import map
 
-
-def get_num_matches(winners_and_picks):
-    return len(set(winners_and_picks[0]) & set(winners_and_picks[1]))
-
-
 print(
     sum(
         open("input.txt", "r").readlines()
@@ -17,7 +12,11 @@ print(
                 [int(s) for s in winners_and_picks[1].strip().split()],
             )
         )
-        | map(get_num_matches)
+        | map(
+            lambda winners_and_picks: len(
+                set(winners_and_picks[0]) & set(winners_and_picks[1])
+            )
+        )
         | map(lambda num_matches: 2 ** (num_matches - 1) if num_matches > 0 else 0)
     )
 )
