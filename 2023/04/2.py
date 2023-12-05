90985244725c713bd5d6f5db1a22413d5c3dae42
Daniel Lantz <daniellantz@macbook-pro.myfiosgateway.com>
2023-12-04 22:16:05 -0500

    part 2

--- 2023/04/2.py
+++ 2023/04/2.py
@@ -1,57 +1,40 @@
-from pipe import map
+from collections import defaultdict
+from pipe import map, Pipe
+from functools import reduce
 
 
-def get_num_matches(winners_and_picks):
-    return len(set(winners_and_picks[0]) & set(winners_and_picks[1]))
+@Pipe
+def pipe_reduce(iterable, function, initializer=None):
+    return reduce(function, iterable, initializer)
 
 
-# print(
-#     list(
-#         open("input.txt", "r").readlines()
-#         | map(lambda line: line.strip().split(":"))
-#         | map(lambda s: (int(s[0].split()[1]), s[1].strip()))
-#         | map(
-#             lambda game_and_numbers: (
-#                 game_and_numbers[0],
-#                 game_and_numbers[1].split("|"),
-#             )
-#         )
-#         | map(
-#             lambda game_and_winners_and_picks: (
-#                 game_and_winners_and_picks[0],
-#                 (
-#                     [int(s) for s in game_and_winners_and_picks[1][0].strip().split()],
-#                     [int(s) for s in game_and_winners_and_picks[1][1].strip().split()],
-#                 ),
-#             )
-#         )
-#         | map(lambda game: get_num_matches(game[1]))
-#     )
-# )
+@Pipe
+def items(d):
+    yield from d.items()
+
+
+def duplicate_card_to_future(acc, curr):
+    acc[curr[0]]
+    for i in range(curr[0] + 1, curr[0] + curr[1] + 1):
+        acc[i] += acc[curr[0]]
+    return acc
 
 
 print(
-    list(
-        open("input.txt", "r").readlines()
-        | map(lambda line: line.strip().split(":"))
-        | map(lambda s: s[1].strip())
-        | map(lambda numbers: numbers.split("|"))
-        | map(
-            lambda winners_and_picks: (
-                [int(s) for s in winners_and_picks[0].strip().split()],
-                [int(s) for s in winners_and_picks[1].strip().split()],
-            )
+    open("input.txt", "r").readlines()
+    | map(lambda line: line.strip().split(":"))
+    | map(lambda s: s[1].strip())
+    | map(lambda numbers: numbers.split("|"))
+    | map(
+        lambda winners_and_picks: (
+            [int(s) for s in winners_and_picks[0].strip().split()],
+            [int(s) for s in winners_and_picks[1].strip().split()],
         )
-        | map(get_num_matches)
-        # | map(lambda num_matches: 2 ** (num_matches - 1) if num_matches > 0 else 0)
     )
+    | map(lambda winners_and_picks: len(set(winners_and_picks[0]) & set(winners_and_picks[1])))
+    | Pipe(enumerate)
+    | pipe_reduce(duplicate_card_to_future, defaultdict(lambda: 1))
+    | items
+    | map(lambda item: item[1])
+    | Pipe(sum)
 )
-"""
-thoughts
-current number is not 0
-
-[4, 2, 2, 1, 0, 0]
-
-index 3 = 1
-true * 2^0 +  true * 2^1 + true * 2^2 + true * 2^3 +  true * 2^4
-"""
